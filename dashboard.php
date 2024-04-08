298c11908136cdbbce6bae9dcefdc651957902e4
reuterShawn <reutershawn45@gmail.com>
2024-04-08 15:21:45 -0500

    Add dashboard

--- dashboard.php
+++ dashboard.php
@@ -10,7 +10,11 @@ include ('includes/header.php');
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-6">
-            Dashboard
+            <h1 class="display-1">Dashboard</h1>
+            <div class="col-md-6">
+                <a href="users.php">Users management </a>
+                <a href="posts.php">Posts management </a>
+            </div>
         </div>
     </div>
 </div>
